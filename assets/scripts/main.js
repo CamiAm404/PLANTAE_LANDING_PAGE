66ea34007f9f359f9a88bf3f06740e94eec96b6c
Diego Castro <diego_castro_soto@hotmail.com>
2023-06-09 21:47:06 -0500

    feat: add javascript functionality

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,15 @@
 /* Archivo para el codigo en javascript */
+function OnScroll(){
+    var header=document.querySelector("header");
+    header.classList.toggle("sticky",window.scrollY>0);
+}
+
+function OnBackToTopClick(){
+    window.scrollTo(0,0);
+}
+
+window.addEventListener("scroll",OnScroll);
+
+window.onload=function(){
+    document.getElementById("back-to-top-img").onclick=OnBackToTopClick;
+}
